35e5f4551244f5aa305d6434b4faf5debce18bf9
Spichkenss <spichkens.2002@mail.ru>
2024-10-02 18:49:41 +0300

    Storybook: changed background colors

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,13 @@ import "../src/styles/index.scss"
 
 const preview: Preview = {
     parameters: {
+        backgrounds: {
+            values: [
+                {name: 'Dark', value: '#1b1c1d'},
+                {name: 'Light', value: '#FFF'},
+            ],
+            default: 'Dark',
+        },
         controls: {
             matchers: {
                 color: /(background|color)$/i,
