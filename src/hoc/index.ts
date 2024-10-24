44df32ae69c589741a79837569a1678c1c070878
Spichkenss <spichkens.2002@mail.ru>
2024-10-24 12:53:32 +0300

    Chore: add public API description

--- src/hoc/index.ts
+++ src/hoc/index.ts
@@ -1,3 +1,10 @@
+/* Публичный API хоков
+* Отсюда экспортируются HOCs, которые будут использоваться
+* в разрабоке интерфейсов.
+*
+* !!! НЕ ЭКСПОРТИРОВАТЬ HOCs, КОТОРЫЕ НУЖНЫ ТОЛЬКО ДЛЯ РАЗРАБОТКИ UI-KIT'a !!!
+*/
+
 export { type WithPolymorphProps, withPolymorphism } from "./withPolymorphism";
 export { type WithThemedStyleProps, withThemedStyle } from "./withThemedStyle";
 export { type WithSlottableProps, slottable } from "./slottable";
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,11 @@
+/* Публичный API хуков
+* Отсюда экспортируются хуки, которые будут использоваться
+* в разрабоке интерфейсов.
+*
+* !!! НЕ ЭКСПОРТИРОВАТЬ ХУКИ,
+* КОТОРЫЕ НУЖНЫ ТОЛЬКО ДЛЯ РАЗРАБОТКИ UI-KIT'a !!!
+*/
+
 export { useSuperKeyDown } from "./useSuperKeyDown";
 export { useKeyDown } from "./useKeyDown";
 export { useEventListener } from "./useEventListener";
